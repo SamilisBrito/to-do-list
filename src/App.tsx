7ad7886532580cf6d9f5625071ef9e7178ca4d64
SamilisBrito <samilisbrito.ufra@gmail.com>
2024-04-12 22:03:55 -0300

    feat: adição do componente TasksContent

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,24 @@
 import styles from "./App.module.css";
 import { Header } from "./components/Header";
 import { InputAddTask } from "./components/InputAddTask";
+import {TasksContent} from './components/TasksContent'
 import "./global.css";
 
 function App() {
   // const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div>
       <Header />
       <main className={styles.mainContent}>
         <header className={styles.headerContent}>
           <InputAddTask />
         </header>
         <section>
-          
+          <TasksContent/>
         </section>
       </main>
-    </>
+    </div>
   );
 }
 
