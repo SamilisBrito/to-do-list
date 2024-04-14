4514f78fa6385328b7fdd64c634b56440a63fa0d
SamilisBrito <samilisbrito.ufra@gmail.com>
2024-04-14 02:48:34 -0300

    adicao de imports, interface, funcionalidades

--- src/components/TasksContent.tsx
+++ src/components/TasksContent.tsx
@@ -1,21 +1,74 @@
+import { TaskProps } from "../App";
 import clipboard from "../assets/clipboard.png";
+import { Task } from "./Task";
 import styles from "./TasksContent.module.css";
 
-export function TasksContent() {
+interface TasksContentProps {
+  tasks: TaskProps[];
+  onDoneTask: (id: string) => void;
+  onDeleteTask: (id: string) => void;
+}
+
+export function TasksContent({
+  tasks,
+  onDoneTask,
+  onDeleteTask,
+}: TasksContentProps) {
+  
+  const pendingTask = tasks.filter((task) => !task.done);
+  const completedTask = tasks.filter((task) => task.done);
+
   return (
     <section>
       <header className={styles.headerContent}>
         <p>
-          Tarefas criadas <span>0</span>
+          Tarefas criadas <span>{tasks.length}</span>
         </p>
-        <p>
-          Concluídas <span>0</span>
-        </p>
+        {completedTask.length === 0 ? (
+          <p>
+            Concluídas <span>{completedTask.length}</span>
+          </p>
+        ) : (
+          <p>
+            Concluídas{" "}
+            <span>
+              {completedTask.length} de {tasks.length}
+            </span>
+          </p>
+        )}
       </header>
-      <div className={styles.taskContainer}>
-        <img src={clipboard} alt="ícone de prancheta" />
-        <strong>Você ainda não tem tarefas cadastradas</strong>
-        <p>Crie tarefas e organize seus itens a fazer</p>
+      <div>
+        {tasks.length === 0 ? (
+          <div className={styles.taskContainer}>
+            <img src={clipboard} alt="ícone de prancheta" />
+            <strong>Você ainda não tem tarefas cadastradas</strong>
+            <p>Crie tarefas e organize seus itens a fazer</p>
+          </div>
+        ) : (
+          <>
+            {pendingTask.map((task) => {
+              return (
+                <Task
+                  key={task.id}
+                  data={task}
+                  onDoneTask={onDoneTask}
+                  onDeleteTask={onDeleteTask}
+                />
+              );
+            })}
+
+            {completedTask.map((task) => {
+              return (
+                <Task
+                  key={task.id}
+                  data={task}
+                  onDoneTask={onDoneTask}
+                  onDeleteTask={onDeleteTask}
+                />
+              );
+            })}
+          </>
+        )}
       </div>
     </section>
   );
