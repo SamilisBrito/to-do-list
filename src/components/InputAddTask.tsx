b444ad2e03ed6e88ee8ba896d1d3bbe228b11679
SamilisBrito <samilisbrito.ufra@gmail.com>
2024-04-14 02:42:59 -0300

    feat: import, interface, estados etc.

--- src/components/InputAddTask.tsx
+++ src/components/InputAddTask.tsx
@@ -1,12 +1,43 @@
-import styles from './InputAddTask.module.css';
 import { PlusCircle } from "phosphor-react";
-export function InputAddTask() {
+import { ChangeEvent, useState } from "react";
+import styles from "./InputAddTask.module.css";
+
+interface InputAddTaskProps {
+  onAddTask: (content: string) => void;
+}
+
+export function InputAddTask({ onAddTask }: InputAddTaskProps) {
+  const [newTask, setNewTask] = useState("");
+
+  function handleContent(event: ChangeEvent<HTMLInputElement>) {
+    setNewTask(event.target.value);
+  }
+
+  function handleAddTask() {
+    if (newTask.trim().length > 0) {
+      onAddTask(newTask);
+      setNewTask("");
+    }
+  }
+
+  const isTaskEmpty = newTask.length === 0;
+
   return (
-    <>
-      <input className={styles.newTask} type="text" id="" placeholder="Adicione uma nova tarefa" />
-      <button className={styles.createButton} type="button">
-        Criar <PlusCircle size={16}/>
+    <form className={styles.form}>
+      <input
+        onChange={handleContent}
+        value={newTask}
+        className={styles.newTask}
+        placeholder="Adicione uma nova tarefa"
+      />
+      <button
+        onClick={handleAddTask}
+        className={styles.createButton}
+        type="submit"
+        disabled={isTaskEmpty}
+      >
+        Criar <PlusCircle size={16} />
       </button>
-    </>
+    </form>
   );
 }
